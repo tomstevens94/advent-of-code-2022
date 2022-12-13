bd6cab7976dd7fe386bc13d52e5e2685035e301a
Thomas Stevens <tommstevens94@gmail.com>
2022-12-13 15:56:58 +0000

    Began day 13 part 1

--- 11-MonkeyInTheMiddle/11-MonkeyInTheMiddle2.js
+++ 11-MonkeyInTheMiddle/11-MonkeyInTheMiddle2.js
@@ -13,33 +13,33 @@ const lines = input.split('\n');
 const monkeys = [];
 
 for (let i = 0; i < lines.length; i += 7) {
-    let monkey = lines.slice(i, i + 6);
-    let obj = {
-        heldItems: [],
-        operation: {
-            type: '',
-            value: 0
-        },
-        divisibleTest: 0,
-        trueThrow: 0,
-        falseThrow: 0
-    };
+  let monkey = lines.slice(i, i + 6);
+  let obj = {
+    heldItems: [],
+    operation: {
+      type: '',
+      value: 0
+    },
+    divisibleTest: 0,
+    trueThrow: 0,
+    falseThrow: 0
+  };
 
-    monkey = monkey.forEach((e, i) => {
-        switch (i) {
-            case 1: obj.heldItems = e.replace('Starting items: ', '').split(', ').map(e => Number(e)); return;
-            case 2: {
-                obj.operation.type = e.split(' ')[6]; 
-                const value = Number(obj.operation.value = e.split(' ')[7].trim()) || 'oldValue'
-                obj.operation.value = value; 
-                return;
-            }
-            case 3: obj.divisibleTest = Number(e.replace('Test: divisible by ', ''))
-            case 4: obj.trueThrow = Number(e.replace('If true: throw to monkey ', ''));
-            case 5: obj.falseThrow = Number(e.replace('If false: throw to monkey ', ''));
-        }
-    })
-    monkeys.push(obj);
+  monkey = monkey.forEach((e, i) => {
+    switch (i) {
+      case 1: obj.heldItems = e.replace('Starting items: ', '').split(', ').map(e => Number(e)); return;
+      case 2: {
+        obj.operation.type = e.split(' ')[6];
+        const value = Number(obj.operation.value = e.split(' ')[7].trim()) || 'oldValue'
+        obj.operation.value = value;
+        return;
+      }
+      case 3: obj.divisibleTest = Number(e.replace('Test: divisible by ', ''))
+      case 4: obj.trueThrow = Number(e.replace('If true: throw to monkey ', ''));
+      case 5: obj.falseThrow = Number(e.replace('If false: throw to monkey ', ''));
+    }
+  })
+  monkeys.push(obj);
 }
 
 let inspections = {};
@@ -47,27 +47,27 @@ let inspections = {};
 monkeys.forEach((_, i) => inspections[i] = 0);
 
 for (let i = 0; i < 10000; i++) {
-    monkeys.forEach((monkey, i) => {
-        if (!monkey.heldItems.length) return;
+  monkeys.forEach((monkey, i) => {
+    if (!monkey.heldItems.length) return;
 
-        const {type: operType, value} = monkey.operation;
-        monkey.heldItems.forEach((worry) => {
-            inspections[i] += 1;
+    const { type: operType, value } = monkey.operation;
+    monkey.heldItems.forEach((worry) => {
+      inspections[i] += 1;
 
-            const adjValue = value === 'oldValue' ? worry : value;
-            let newWorry = operType === '*' ? worry * adjValue : worry + adjValue;
-            // newWorry = Math.floor(newWorry / 3);
+      const adjValue = value === 'oldValue' ? worry : value;
+      let newWorry = operType === '*' ? worry * adjValue : worry + adjValue;
+      // newWorry = Math.floor(newWorry / 3);
 
-            const testIsTrue = newWorry % monkey.divisibleTest === 0;
-            if (testIsTrue) {
-                monkeys[monkey.trueThrow].heldItems.push(newWorry);
-            } else {
-                monkeys[monkey.falseThrow].heldItems.push(newWorry);
-            }
-        });
-
-        monkey.heldItems = [];
+      const testIsTrue = newWorry % monkey.divisibleTest === 0;
+      if (testIsTrue) {
+        monkeys[monkey.trueThrow].heldItems.push(newWorry);
+      } else {
+        monkeys[monkey.falseThrow].heldItems.push(newWorry);
+      }
     });
+
+    monkey.heldItems = [];
+  });
 }
 
 // console.log(monkeys)
