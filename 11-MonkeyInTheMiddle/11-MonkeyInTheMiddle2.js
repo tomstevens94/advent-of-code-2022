4551fe51679e6eaba8c32e926397ec53e408efd9
Tom <tommstevens94@gmail.com>
2022-12-15 17:32:53 +0000

    Day 11 part 2 finish

--- 11-MonkeyInTheMiddle/11-MonkeyInTheMiddle2.js
+++ 11-MonkeyInTheMiddle/11-MonkeyInTheMiddle2.js
@@ -6,8 +6,6 @@ require.extensions['.txt'] = function (module, filename) {
 
 const input = require("./input.txt");
 
-// console.log(input);
-
 const lines = input.split('\n');
 
 const monkeys = [];
@@ -27,55 +25,57 @@ for (let i = 0; i < lines.length; i += 7) {
 
     monkey = monkey.forEach((e, i) => {
         switch (i) {
-            case 1: obj.heldItems = e.replace('Starting items: ', '').split(', ').map(e => BigInt(e)); return;
+            case 1: obj.heldItems = e.replace('Starting items: ', '').split(', ').map(e => Number(e)); return;
             case 2: {
                 obj.operation.type = e.split(' ')[6];
                 const value = Number(e.split(' ')[7].trim());
-                obj.operation.value = value ? BigInt(value) : 'oldValue'; 
+                obj.operation.value = value ? Number(value) : 'oldValue'; 
                 return;
             }
-            case 3: obj.divisibleTest = BigInt(e.replace('Test: divisible by ', ''));
+            case 3: obj.divisibleTest = Number(e.replace('Test: divisible by ', ''));
             case 4: obj.trueThrow = Number(e.replace('If true: throw to monkey ', ''));
             case 5: obj.falseThrow = Number(e.replace('If false: throw to monkey ', ''));
         }
     })
     monkeys.push(obj);
 }
+const commonFactor = monkeys.map(e => e.divisibleTest).reduce((total, num) => total * num, 1);
 
 let inspections = {};
 
 monkeys.forEach((_, i) => inspections[i] = 0);
+console.log(monkeys)
+const loopCount = 10000;
+for (let i = 0; i < loopCount; i++) {
+  monkeys.forEach((monkey, i) => {
+    if (!monkey.heldItems.length) return;
 
-for (let i = 0; i < 100; i++) {
-    monkeys.forEach((monkey, i) => {
-        if (!monkey.heldItems.length) return;
+    const { type, value } = monkey.operation;
 
-    const { type: operType, value } = monkey.operation;
     monkey.heldItems.forEach((worry) => {
       inspections[i] += 1;
 
-            const adjValue = value === 'oldValue' ? worry : value;
-            
-            let newWorry = operType === '*' ? worry * adjValue : worry + adjValue;
+      const adjValue = value === 'oldValue' ? worry : value;
 
-            const testIsTrue = newWorry % adjValue == 0;
-            if (testIsTrue) {
-                monkeys[monkey.trueThrow].heldItems.push(newWorry);
-            } else {
-                monkeys[monkey.falseThrow].heldItems.push(newWorry);
-            }
-        });
+      let newWorry = type === '*' ? worry * adjValue : worry + adjValue;
 
-        monkey.heldItems = [];
+      const testIsTrue = newWorry % monkey.divisibleTest === 0;
+      if (testIsTrue) {
+          monkeys[monkey.trueThrow].heldItems.push(newWorry % commonFactor);
+      } else {
+          monkeys[monkey.falseThrow].heldItems.push(newWorry % commonFactor);
+      }
+    
+      monkey.heldItems = [];
     });
 
     monkey.heldItems = [];
   });
 }
 
-console.log(monkeys)
-// console.log(inspections);
-console.log(Object.values(inspections).sort((a, b) => b - a).slice(0, 2).reduce((total, num) => total * num, 1)); // Part 1
+const sortedInspections = Object.values(inspections).sort((a, b) => b - a);
+const firstTwo = sortedInspections.slice(0, 2);
+console.log('MONEY BUSINESS: ', firstTwo.reduce((total, num) => total * num, 1)); // Part 2
 
-// < 14391121333
-// not 14398800024
+// Tried:
+// 32045296119
